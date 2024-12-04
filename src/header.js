b2ff17ffe6704a9b968e0e485193733bb1b63bdf
Sagetel <102986847+Sagetel@users.noreply.github.com>
2024-12-04 12:19:47 +0300

    lesson_2

--- src/header.js
+++ src/header.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react'
+import NavBar from './navBar'
 
 export default class Header extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      email: ''
+      email: '',
+      includesReact: false,
     }
+    this.navItems = ['Главная страница', 'личный кабинет', 'Войти', 'Выйти']
+    this.myRef = React.createRef();
   };
   handleInputChange = (event) => {
-    console.log(this.state)
+    const newValue = event.target.value
+    if (newValue.search('реакт') !== -1) {
+      this.setState({
+        includesReact: true,
+      });
+    }
     this.setState({
-      email: event.target.value,
+      email: newValue,
     });
   };
-  componentDidMount() {
-    console.log('Я смонтировался')
-  }
-  componentDidUpdate() {
-    console.log('Я обновился')
-  }
-  UNSAFE_componentWillUpdate() {
-    console.log('Я буду обновлятсья')
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  
 
-  }
+  // componentDidMount() {
+  //   console.log('Я смонтировался')
+  // }
+  // componentDidUpdate() {
+  //   console.log('Я обновился')
+  // }
+  // UNSAFE_componentWillUpdate() {
+  //   console.log('Я буду обновлятсья')
+  // }
 
   render() {
     return (
       <div>
-        <form action="">
-          <input type="email" name="email" id="" value={this.state.email}
-            onChange={this.handleInputChange} />
-          <input type="button" value="" />
+        <form action="" onSubmit={this.handleSubmit}>
+          <input type="email" name="email" value={this.state.email}
+            onChange={this.handleInputChange}
+            ref={this.myRef} />
+          <button type="submit" disabled={this.state.includesReact}>Отправить</button>
+          <button type="button" onClick={() => { this.myRef.current.focus(); }}>Фокус</button>
         </form>
+        <NavBar items={this.navItems}/>
       </div>
     )
   }
